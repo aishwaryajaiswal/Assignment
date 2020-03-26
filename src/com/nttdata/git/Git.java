f5356729bea83892cc93db100d36d77866c7b856
Aishwarya Jaiswal <ishuaj10@gmail.com>
2020-03-26 15:08:58 +0530

    First Commit

---
+++ src/com/nttdata/git/Git.java
@@ -0,0 +1,11 @@
+package com.nttdata.git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi This is GIT");
+
+	}
+
+}
---
+++ src/com/nttdata/git/gitexample.java
@@ -0,0 +1,11 @@
+package com.nttdata.git;
+
+public class gitexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Git");
+
+	}
+
+}
